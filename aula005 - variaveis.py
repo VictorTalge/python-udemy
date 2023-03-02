d5e5d29725659c57c01e9d514394750368a687e4
Victor Talge <vt.talge@gmail.com>
2023-03-02 20:49:38 -0300

    Aula 5

---
+++ aula005 - variaveis.py
@@ -0,0 +1,21 @@
+x = 5
+y = "string"
+w = 10.00
+a = str(3) #a será "3"
+b = int(3) #b será 3 inteiro
+c = float(3) #c será 3.0
+#python é case sensitive, letras maiusculas e minusculas são diferentes
+d = 4
+D = "alfa"
+
+print(x)
+print(y)
+print(w)
+print(type(a))
+print(a)
+print(type(b))
+print(b)
+print(type(c))
+print(c)
+print(d)
+print(D)
