c0bd5092cecf8cc3dd31d66d993270388ce37abf
Victor Talge <vt.talge@gmail.com>
2023-03-02 20:30:25 -0300

    Aula 4

--- aula03.py
+++ aula03.py
@@ -1,5 +1,12 @@
+#Comentario
 if 5 > 2:
-    print("5 é maior que 2")
+    print("5 é maior que 2") # Comentario em uma linha de codigo
 else:
     print("5 é menor que 2")
 print("Fim")
+
+"""
+Bloco 
+de 
+comentário
+"""
