0c1f9f683cfe34dd1733933de016a66aa0a54e9f
Victor Talge <vt.talge@gmail.com>
2023-05-28 14:28:47 -0300

    Exercicio 1

---
+++ Exercicios/Armazenamentos/Armazenamentos.py
@@ -0,0 +1,19 @@
+#Declaração das variáveris do programa
+
+d = 0
+print("O valor da variável D é:", d)
+
+a = 2
+print("O valor da variável A é:", a)
+
+b = 3
+print("O valor da variável B é:", b)
+
+c = 5
+print("O valor da variável C é:", c)
+
+c = c+1
+print("O valor novo da variável C é:", c)
+
+d = a + c
+print("O valor novo da variável D é:", d)
