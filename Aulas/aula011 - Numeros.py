0805cac57bf6ef7d3e78fa700fbdef065b6bdccd
Victor Talge <vt.talge@gmail.com>
2023-07-17 22:18:45 -0300

    aula 14

---
+++ Aulas/aula011 - Numeros.py
@@ -0,0 +1,53 @@
+import random
+
+a = 1
+b = 123456789
+c = -345678
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+d = 1.25
+e = 1.0
+f = -35.9
+
+print(type(d))
+print(type(e))
+print(type(f))
+
+g = 35e3
+h = 18E7
+i = -16.7e8
+
+print(type(g))
+print(type(h))
+print(type(i))
+
+j = 3+5j
+k = 5j
+l = -5j
+
+print(type(j))
+print(type(k))
+print(type(l))
+
+x = float(1)
+
+y = int(1.8)
+
+z = complex(y)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+num = random.randrange(1, 10)
+
+print(num)
+print(random.randrange(1, 10))
+print("\n")
