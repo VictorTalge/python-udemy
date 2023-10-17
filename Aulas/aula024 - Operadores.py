548d50095eceb30d0a3c4708fe8e91d2d6e970e7
Victor Talge <vt.talge@gmail.com>
2023-10-16 22:50:16 -0300

    Aula 27

---
+++ Aulas/aula024 - Operadores.py
@@ -0,0 +1,25 @@
+x = 10
+y = 2
+z = x + y 
+print(z)
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+
+x = 2
+y = 5
+
+print(x ** y)
+
+x = 15
+y = 2
+
+print(x // y)
+
+a = 5 + 2 * 4
+print(a)
+
+a = (5 + 2) * 4
+print(a)
